9689672f4118b9c2d1cc96ebb2f91f526c2824d7
Aparna Jha <aparnajha482@gmail.com>
2018-03-08 09:38:00 +0530

    drawn a pot below by using attributes of turtle.

--- mini_project_TURTLE.py
+++ mini_project_TURTLE.py
@@ -7,12 +7,29 @@ def draw_square(romeo):
         romeo.right(90)
 
 
-def draw_graphic()
+def draw_graphic():
     brad = turtle.Turtle()
     window = turtle.Screen()
     window.bgcolor("pink")
     brad.color("green")
     brad.shape("turtle")
-    for i in range(1,72):
+    brad.speed(20)
+    for i in range(1,73):
         draw_square(brad)
         brad.right(5)
+
+    brad.right(90)
+    brad.forward(200)
+    brad.right(90)
+    brad.forward(90)
+    brad.left(110)
+    brad.forward(100)
+    brad.left(70)
+    brad.forward(100)
+    brad.left(70)
+    brad.forward(100)
+    brad.left(110)
+    brad.forward(70)
+
+    window.exitonclick()
+
